6abed6015da8532251ba4f21ec8a10ceb5a2ef28
ndchoate <ndchoate@gmail.com>
2020-01-25 17:46:11 -0500

    Add react frontend

---
+++ frontend/react_spa/src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+import "./index.css";
+
+ 
+ReactDOM.render(
+  <Main/>, 
+  document.getElementById("root")
+);
